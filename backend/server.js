36d9b95c4e9e4496a35bb03001fefa0a84bee50c
Kelli Eng <128423077+kelliceng@users.noreply.github.com>
2025-04-20 12:32:58 -0400

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
 const cors = require('cors');
 const postRoutes = require('./routes/posts');
+
+dotenv.config();
 
 const app = express();
 
@@ -14,12 +15,12 @@ app.use('/api/posts', postRoutes);
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.use((req, res, next) => {
-    console.log(`🔥 Received ${req.method} request to ${req.originalUrl}`);
-    next();
+  console.log(`🔥 Received ${req.method} request to ${req.originalUrl}`);
+  next();
 });
 
 const PORT = process.env.PORT || 5050;
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`)))
-  .catch(err => console.log(err));
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch(err => console.error('MongoDB connection error:', err));
